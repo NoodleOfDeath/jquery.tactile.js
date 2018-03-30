5967e10c162e58399fb913b5668f4572b0c1c85c
Thom Morgan <thom@noodleofdeath.com>
2018-03-30 11:49:29 -0400

    Add files via upload

--- js/jquery.tactile.js
+++ js/jquery.tactile.js
@@ -8,54 +8,113 @@
 
 (function ($) {
 	
+	/**
+	 * jQuery plugin that can make a DOM element draggable and/or resizable.
+	 */
 	$.fn.tactile = function(config) {
 		
-		function releaseTactileControl(container) {
+		function enableSelection(container, enable) {
+			if (enable == null) enable = true;
+			var attr = enable ? 'auto' : 'none';
+			$(container).css({
+				'-webkit-touch-callout' : attr,
+	    	    '-webkit-user-select' : attr,
+	    	    '-khtml-user-select' : attr,
+	    	    '-moz-user-select' : attr,
+	    	    '-ms-user-select' : attr,
+	    	    'user-select' : attr,
+			});
+		}
 
-			$(document.body).enableSelection();
-			container.enableMouseEvents();
+		function disableSelection(container, disable) {
+			if (disable == null) disable = true;
+			enableSelection(container, !disable);
+		}
+		
+		function enableMouseEvents(container, enable) {
+			if (enable == null) enable = true;
+			var attr = enable ? 'auto' : 'none';
+			$(container).css({
+				'pointer-events' : attr,
+			});
+		}
+		
+		function disableMouseEvents(container, disable) {
+			if (disable == null) disable = true;
+			enableMouseEvents(container, !disable);
+		}
+		
+		function release(container) {
+
+			enableSelection(document.body);
+			enableMouseEvents(container);
 			
 		    containerOrigin = null;
-			mousedownOrigin = null;
-			mousedownOffset = null;
+		    containerDimens = null;
+		    
+			mousedownOriginDrag = null;
+			mousedownOffsetDrag = null;
 			
-			if (container.offset().left < 0) {
-				container.offset({ 
-					left : 10, 
-					top: container.offset().top, 
-				});
-			} else if (container.offset().left > $(window).width() - container.outerWidth()) {
-				container.offset({ 
-					left : container.offset().left = $(window).width() - container.outerWidth() - 10,
-					top: container.offset().top, 
-				});
-			}
-
-			if (container.offset().top < 0) {
-				container.offset({ 
-					left : container.offset().left, 
-					top: 10, 
-				});
-			} else if (container.offset().top > $(window).height() - container.outerHeight()) {
-				container.offset({ 
-					left : container.offset().left, 
-					top : container.offset().top = $(window).height() - container.outerHeight() - 10,
-				});
-			}
+			mousedownOriginResize = null;
+			mousedownOffsetResize = null;
+			
+			adjustVisibility(container);
 			
 		}
 		
-		var mousedownOrigin = null;
-		var mousedownOffset = null;
-		var containerOrigin = null;
-		var containerDimens = null;
-		
-		this.autogenerateHandle = true;
+		function adjustVisibility(container) {
+
+			if (containerMargin == null) {
+				containerMargin = {
+					top : 10,
+					right : 10,
+					bottom: 10,
+					left: 10,
+				}
+			} else
+			if (typeof containerMargin == 'number') {
+				containerMargin = {
+					top : containerMargin,
+					right : containerMargin,
+					bottom: containerMargin,
+					left: containerMargin,
+				}
+			}
+			
+			var dxleft = container.offset().left;
+			var dxtop = container.offset().top;
+			
+			if (container.offset().left < $(document).scrollLeft()) {
+				dxleft = containerMargin.left + $(document).scrollLeft();
+			} else if (container.offset().left + container.outerWidth() > $(window).width() + $(document).scrollLeft()) {
+				dxleft = $(window).width() - container.outerWidth() - containerMargin.right + $(document).scrollLeft();
+			}
+			
+			if (container.offset().top < $(document).scrollTop()) {
+				dxtop = containerMargin.top + $(document).scrollTop();
+			} else if (container.offset().top + container.outerHeight() > $(window).height() + $(document).scrollTop()) {
+				dxtop = $(window).height() - container.outerHeight() - containerMargin.bottom + $(document).scrollTop();
+			}
+			
+			container.offset({
+				left : dxleft,
+				top: dxtop, 
+			})
+			
+		}
     	
     	if (config == null)
     		config = {};
     	
+		var containerOrigin = null;
+		var containerDimens = null;
+		
+		var containerMargin = config.containerMargin;
+    	
     	if (config.draggable != null) {
+    		
+    		var mousedownOriginDrag = null;
+    		var mousedownOffsetDrag = null;
     		
     		var container = config.draggable.container;
     		var handle = config.draggable.handle;
@@ -83,8 +142,8 @@
 	    	handle.addClass("draggable-handle");
 	    	
 	    	handle.off("mousedown.draggable").on("mousedown.draggable", function(e) {
-	    		$(document.body).disableSelection();
-	    		mousedownOrigin = {
+	    		disableSelection(document.body);
+	    		mousedownOriginDrag = {
 	    			left: e.clientX,
 	    			top: e.clientY,
 	    		};
@@ -95,46 +154,43 @@
 	    	});
 	    	
 	    	$(document.body).off("mousemove.draggable").on("mousemove.draggable", function(e) {
-	    		if (mousedownOrigin == null) return;
-	    		if (mousedownOffset == null)
-	    			container.disableMouseEvents();
-	    		mousedownOffset = {
-	    			left: e.clientX - mousedownOrigin.left,
-	    			top: e.clientY - mousedownOrigin.top,
+	    		if (mousedownOriginDrag == null) return;
+	    		if (mousedownOffsetDrag == null)
+	    			disableMouseEvents(container);
+	    		mousedownOffsetDrag = {
+	    			left: e.clientX - mousedownOriginDrag.left,
+	    			top: e.clientY - mousedownOriginDrag.top,
 	    		}
 	    		container.offset({
-	    			left: containerOrigin.left + mousedownOffset.left,
-	    			top: containerOrigin.top + mousedownOffset.top,
+	    			left: containerOrigin.left + mousedownOffsetDrag.left,
+	    			top: containerOrigin.top + mousedownOffsetDrag.top,
 	    		});
 	    	});
 	    	
 	    	$(document.body).off("mouseup.draggable mouseleave.draggable").on("mouseup.draggable mouseleave.draggable", function(e) {
-	    		releaseTactileControl(container);
+	    		if (mousedownOriginDrag == null) return;
+	    		release(container);
+	    	});
+	    	
+	    	$(window).off('resize.draggable').on('resize.draggable', function(e) {
+	    		adjustVisibility(container);
 	    	});
     	
     	}
     	
-    	/*
     	if (config.resizable != null) {
     		
+    		var mousedownOriginResize = null;
+    		var mousedownOffsetResize = null;
+    		
     		var container = config.resizable.container;
+    		
+    		var minWidth = config.resizable.minWidth;
+    		var minHeight = config.resizable.minHeight;
+    		
     		var handle = config.resizable.handle;
-    		var handleContainer = config.resizable.handleContainer;
     		
-    		if (typeof handleContainer == 'string') handleContainer = container.find(handleContainer);
-    		if (handleContainer == null || handleContainer.length == 0) handleContainer = $(handleContainer);
-    		
-    		if (handleContainer.length == 0) {
-    			handleContainer = $(document.createElement("div"));
-    			handleContainer.addClass("handle-container");
-    		}
-    		
-    		if (config.resizable.handleBefore != null)
-    			$(config.resizable.handleBefore).before(handleContainer);
-    		else if (config.resizable.handleAfter != null)
-    			$(config.resizable.handleAfter).after(handleContainer);
-    		
-    		$(handleContainer).after('<div class="clearfix"></div>');
+    		var onResize = config.onResize;
     		
     		if (typeof container == 'string')
         		container = $(this).find(container);
@@ -153,18 +209,11 @@
 	    	if (handle == null)
 	    		handle = $(handle);
 	    	
-	    	if (handle.length == 0) {
-	    		handle = $(document.createElement("div"));
-	    		handle.append('<i class="fa fa-expand"></i>');
-	    		handle.addClass("generated-handle");
-	    		handleContainer.append(handle);
-	    	}
-	    	
-	    	handle.addClass("resizable-handle");
+	    	handle.addClass("resize-handle");
 	    	
 	    	handle.off("mousedown.resizable").on("mousedown.resizable", function(e) {
-	    		$(document.body).disableSelection();
-	    		mousedownOrigin = {
+	    		disableSelection(document.body);
+	    		mousedownOriginResize = {
 	    			left: e.clientX,
 	    			top: e.clientY,
 	    		};
@@ -179,59 +228,44 @@
 	    	});
 	    	
 	    	$(document.body).off("mousemove.resizable").on("mousemove.resizable", function(e) {
-	    		if (mousedownOrigin == null) return;
-	    		if (mousedownOffset == null)
-	    			container.disableMouseEvents();
-	    		mousedownOffset = {
-	    			left: e.clientX - mousedownOrigin.left,
-	    			top: e.clientY - mousedownOrigin.top,
+	    		if (mousedownOriginResize == null) return;
+	    		if (mousedownOffsetResize == null)
+	    			disableMouseEvents(container);
+	    		mousedownOffsetResize = {
+	    			left: e.clientX - mousedownOriginResize.left,
+	    			top: e.clientY - mousedownOriginResize.top,
+	    		}
+	    		var dxleft = containerOrigin.left;
+	    		var dxtop =  containerOrigin.top;
+	    		var width = containerDimens.width + mousedownOffsetResize.left;
+	    		var height = containerDimens.height + mousedownOffsetResize.top;
+	    		if (width >= (minWidth || 400) && container.offset().left + width < $(window).width() + $(document).scrollLeft()) {
+	    			dxleft = containerOrigin.left - (mousedownOffsetResize.left / 2);
+	    			container.width(width);
+	    		} else {
+	    			width = (minWidth || 400);
+	    		}
+	    		if (height >= (minHeight || 320) && container.offset().top + height < $(window).height() + $(document).scrollTop()) {
+	    			dxtop = containerOrigin.top - (mousedownOffsetResize.top / 2);
+	    			container.height(height);
+	    		} else {
+	    			height = (minHeight || 320);
 	    		}
 	    		container.offset({
-	    			left: containerOrigin.left - (mousedownOffset.left / 2),
-	    			top: containerOrigin.top - (mousedownOffset.top / 2),
+	    			left: dxleft,
+	    			top: dxtop,
 	    		});
-	    		container.size({
-	    			width: container.outerWidth() + (mousedownOffset.left / 2),
-	    			height: container.outerHeight() + (mousedownOffset.top / 2),
-	    		});
+	    		if (typeof onResize == 'function') onResize(width, height);
 	    	});
 	    	
 	    	$(document.body).off("mouseup.resizable mouseleave.resizable").on("mouseup.resizable mouseleave.resizable", function(e) {
-	    		releaseTactileControl(container, "resizable");
+	    		release(container);
 	    	});
     		
-    	}*/
+    	}
     	
     	return this;
     	
     };
-	
-	$.fn.enableSelection = function(enable) {
-		if (enable == null) enable = true;
-		var attr = enable ? 'auto' : 'none';
-		$(this).css({
-			'-webkit-touch-callout': attr,
-    	    '-webkit-user-select': attr,
-    	    '-khtml-user-select': attr,
-    	    '-moz-user-select': attr,
-    	    '-ms-user-select': attr,
-    	    'user-select': attr,
-		});
-	}
-
-	$.fn.disableSelection = function(disable) {
-		if (disable == null) disable = true;
-		$(this).enableSelection(!disable);
-	}
-	
-	$.fn.enableMouseEvents = function(enable) {
-		if (enable == null) enable = true;
-		$(this).css('pointer-events', enable ? 'auto' : 'none');
-	}
-	
-	$.fn.disableMouseEvents = function(disable) {
-		if (disable == null) disable = true;
-		$(this).enableMouseEvents(!disable);
-	}
     
 }(jQuery));
